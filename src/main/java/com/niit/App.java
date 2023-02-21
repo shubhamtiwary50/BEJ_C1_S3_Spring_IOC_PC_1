1fcf3c6d60309f6f792cca07f7a413be026274be
SHUBHAM <shubhamtiwary50@gmail.com>
2023-02-21 18:03:22 +0530

    The implementation class.

--- src/main/java/com/niit/App.java
+++ src/main/java/com/niit/App.java
@@ -1,5 +1,10 @@
 package com.niit;
 
+import com.niit.config.BeanConfig;
+import com.niit.domain.Employee;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+//        System.out.println( "Hello World!" );
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanConfig.class);
+        Employee employee= applicationContext.getBean(Employee.class);
+        System.out.println(employee);
     }
 }
