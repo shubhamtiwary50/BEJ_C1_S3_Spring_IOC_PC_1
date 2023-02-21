b7f60ad00403afa091aa0fdd5e456f2300c71498
SHUBHAM <shubhamtiwary50@gmail.com>
2023-02-21 17:55:38 +0530

    Done with the bean config file only minor changes remain now.

--- src/main/java/com/niit/config/BeanConfig.java
+++ src/main/java/com/niit/config/BeanConfig.java
@@ -1,4 +1,18 @@
 package com.niit.config;
 
+import com.niit.domain.Department;
+import com.niit.domain.Employee;
+import org.springframework.context.annotation.Bean;
+
 public class BeanConfig {
+
+    @Bean
+    public Employee getEmployee(){
+        return new Employee();
+    }
+
+    @Bean
+     public Department getDepartment(){
+         return new Department("IOC Department","6.0.5");
+     }
 }
