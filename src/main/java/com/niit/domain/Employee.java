ea1f9287f2be2e773c0984c27fd8d6654665460a
SHUBHAM <shubhamtiwary50@gmail.com>
2023-02-21 17:47:10 +0530

    Populated the Employee class with constructor, getter setter, toString, hashcode and equals.

--- src/main/java/com/niit/domain/Employee.java
+++ src/main/java/com/niit/domain/Employee.java
@@ -1,9 +1,76 @@
 package com.niit.domain;
 
+import java.util.Objects;
+
 public class Employee {
 
     private int empId;
     private String empName;
     private String empAddress;
+    private Department department;
 
+    public Employee() {
+    }
+
+    public Employee(int empId, String empName, String empAddress, Department department) {
+        this.empId = empId;
+        this.empName = empName;
+        this.empAddress = empAddress;
+        this.department = department;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getEmpAddress() {
+        return empAddress;
+    }
+
+    public void setEmpAddress(String empAddress) {
+        this.empAddress = empAddress;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee)) return false;
+        Employee employee = (Employee) o;
+        return getEmpId() == employee.getEmpId() && Objects.equals(getEmpName(), employee.getEmpName()) && Objects.equals(getEmpAddress(), employee.getEmpAddress()) && Objects.equals(getDepartment(), employee.getDepartment());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getEmpId(), getEmpName(), getEmpAddress(), getDepartment());
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empId=" + empId +
+                ", empName='" + empName + '\'' +
+                ", empAddress='" + empAddress + '\'' +
+                ", department=" + department +
+                '}';
+    }
 }
